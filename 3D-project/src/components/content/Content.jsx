186dad1a8d33d360c0ecac12089e577de1f59d8b
AstrO <kostadinov226@abv.bg>
2024-02-10 12:15:45 +0200

    Added react-router-dom
    Responsive Changes

--- 3D-project/src/components/content/Content.jsx
+++ 3D-project/src/components/content/Content.jsx
@@ -15,7 +15,7 @@ const Content = () => {
       <div className='text-content'>
         <h1 className='Model'>3D <span className='color1'>MODEL</span></h1>
         <h4><span className='colors1'>THE</span> BEAUTY OF 3D MODELS</h4>
-        <p className='general-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure corporis ex praesentium nulla! Expedita temporibus incidunt eius totam alias excepturi asperiores voluptas labore. Voluptates, ipsam ad officia aut culpa dolorem?</p>
+        <p className='general-text'>Use Spline to quickly and easily create your own 3D model to decorate your website. With Spline, you can create unique shapes and abstract images that capture the attention of your visitors and make them feel amazed by the visual experience</p>
         <button onClick={redirectToSite}>Check in</button>
        <div className='custom'><Spline scene="https://prod.spline.design/oS9izNe3zgwhP2QU/scene.splinecode"></Spline></div>
       </div>
